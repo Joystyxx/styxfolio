2297507300759129bffa4b6316cfb17a27ac9525
JoyStyx <111282646+Joystyxx@users.noreply.github.com>
2026-09-11 01:32:48 +0100

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,9 +3,9 @@ export const CONFIG = {
   // Site Settings
   // ---------------------------------------------------------------------------
   site: {
-    url: "https://alexmercer.dev",
+    url: "https://olawale-afolayan.vercel.app",
     locale: "en_US",
-    twitterHandle: "@alexmercer_dev",
+    twitterHandle: "@joymiide",
   },
 
   // ---------------------------------------------------------------------------
